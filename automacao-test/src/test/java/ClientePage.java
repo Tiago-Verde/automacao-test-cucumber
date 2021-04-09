2883b23ea29c65dff28cbd88ca9625a9aaee7157
Tiago-Verde <vutu.goes@gmail.com>
2021-04-08 21:34:49 -0300

    alteração metodo acessarSistema()

--- automacao-test/src/test/java/ClientePage.java
+++ automacao-test/src/test/java/ClientePage.java
@@ -1,8 +1,21 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
+
+import io.cucumber.java.Before;
 
 public class ClientePage extends ClienteElementMap {
 
+	private static WebDriver driver;
+
+	
+	@Before
 	public void acessarSistema() {
+
+		System.setProperty("webdriver.chrome.driver", "C:/Tools/Drivers/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.navigate().to("http://test.juniorsbrissa.com/pagina-de-exemplo/");
 
 	}
 
